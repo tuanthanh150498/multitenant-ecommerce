18e0eab93e3d3d993fc83445cf28db389689f71e
Thanh Tuan <tuanthanhvip150498@gmail.com>
2025-07-03 23:27:39 +0700

    004: Payload Integration

---
+++ src/collections/Categories.ts
@@ -0,0 +1,12 @@
+import { CollectionConfig } from "payload";
+
+export const Categories: CollectionConfig = {
+  slug: "categories",
+  fields: [
+    {
+      name: "name",
+      type: "text",
+      required: true,
+    },
+  ],
+};
