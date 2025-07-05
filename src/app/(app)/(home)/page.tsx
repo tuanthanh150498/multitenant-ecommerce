1c8017f6a467200bb0b4f32a5762ad66922a65b1
Thanh Tuan <tuanthanhvip150498@gmail.com>
2025-07-05 19:19:00 +0700

    005: Search Filters

--- src/app/(app)/(home)/page.tsx
+++ src/app/(app)/(home)/page.tsx
@@ -4,19 +4,8 @@
 // import { Checkbox } from "@/components/ui/checkbox";
 // import { Progress } from "@/components/ui/progress";
 
-import configPromise from "@payload-config";
-import { getPayload } from "payload";
-
-export default async function Home() {
-  const payload = await getPayload({
-    config: configPromise,
-  });
-
-  const data = await payload.find({
-    collection: "categories",
-  });
-
-  return <div>{JSON.stringify(data, null, 2)}</div>;
+export default function Home() {
+  return <div>Home Page</div>;
   //return (
   // <div className="p-4">
   //   <div className="flex flex-col gap-y-4">
